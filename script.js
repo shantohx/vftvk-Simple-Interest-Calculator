1091d08ab6a57e98a7150153af6d13c9ddbb9d02
shantohx <shantohx@gmail.com>
2021-11-21 05:17:02 +0600

    Complete work

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+function changeRate()
+{
+    var rateval = document.getElementById("rate").value;
+    var rate = rateval+"%"
+    document.getElementById("rate_val").innerHTML = rate;
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = principal*rate*years / 100;
     
+    // var total = parseFloat(principal) + parseFloat(interest);
+
+    var year = new Date().getFullYear()+parseInt(years);
+    
+    var mes = "If you deposit <mark>"+ principal+ "</mark>,<br /> at an interest rate of <mark>" 
+    + rate +"%</mark>,<br /> You will receive an amount of <mark>" + interest + "</mark>, <br /> in the year <mark>" + year + "</mark>";
+    document.getElementById("result").innerHTML = mes;
+
 }
-        
